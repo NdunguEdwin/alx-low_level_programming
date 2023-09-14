39642ffd3bb9d9dbab540db551858f193f77af89
NdunguEdwin <edwinndungu295@gmail.com>
2023-09-14 12:46:10 -0700

    adding arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * add_them_all - calculattes the sum of all its parameters
+ * sum_them_all - calculattes the sum of all its parameters
  * @n: number of arguments passed to the function
  *
  * Return: the sum
