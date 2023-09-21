ea186bb763746e8ef33eb710a4761b43e12d4035
NdunguEdwin <edwinndungu295@gmail.com>
2023-09-21 12:55:23 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+nclude <stdlib.h>
+#include "lists.h"
+/**
+ *  * list_len - returns the length of linked list
+ *   * @h: head pointer
+ *    * Return: size of linked list
+ *     */
+size_t list_len(const list_t *h)
+{
+		size_t len = 0;
+
+			while (h)
+					{
+								len++;
+										h = h->next;
+											}
+
+				return (len);
+}
